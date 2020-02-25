3f8776affa5aaeb16bbdf7c01ebc59b23c96103a
Teis <teisgossen@gmail.com>
2020-02-25 14:25:48 +0100

    Code cleanup
    + Added functions to make the code more readible
    + FixedUpdate() now only has functions
    
    - Removed unnecessary code

--- Player_Movement_2D.cs
+++ Player_Movement_2D.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class Player_Movement_2D : MonoBehaviour {
 
     private Rigidbody2D rigidBody2D;
@@ -10,7 +10,6 @@ public class Player_Movement_2D : MonoBehaviour {
     [SerializeField] private float jump_Force;
     [SerializeField] private float fall_multiplier;
     [SerializeField] private float low_jump_multiplier;
-    //[SerializeField] private float movement_scalar;
 
     [SerializeField] private bool is_grounded = false;
     [SerializeField] private Transform is_ground_checker;
@@ -32,18 +31,35 @@ public class Player_Movement_2D : MonoBehaviour {
 
     // FixedUpdate is called at a fixed time interval
     void FixedUpdate() {
+        Move();
+        Jump();
+        BetterJump();
+        CheckIfGrounded();
+    }
+
+    private void Move() {
         float x_movement = Input.GetAxis("Horizontal");
-
         float move_by = x_movement * max_speed;
-
         rigidBody2D.velocity = new Vector2(move_by, rigidBody2D.velocity.y);
+    }
 
+    private void Jump() {
         if (Input.GetKey(KeyCode.Space) && (is_grounded || Time.time - last_time_grounded <= remember_grounded_for)) {
             rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jump_Force);
         }
+    }
 
+    private void BetterJump() {
+        if (rigidBody2D.velocity.y < 0) {
+            rigidBody2D.velocity += Vector2.up * Physics2D.gravity * (fall_multiplier - 1) * Time.deltaTime;
+        }
+        else if (rigidBody2D.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
+            rigidBody2D.velocity += Vector2.up * Physics2D.gravity * (low_jump_multiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void CheckIfGrounded() {
         Collider2D collider = Physics2D.OverlapCircle(is_ground_checker.position, check_ground_radius, ground_layer);
-
         if (collider != null) {
             is_grounded = true;
         }
@@ -53,16 +69,5 @@ public class Player_Movement_2D : MonoBehaviour {
             }
             is_grounded = false;
         }
-
-        if (rigidBody2D.velocity.y < 0) {
-            rigidBody2D.velocity += Vector2.up * Physics2D.gravity * (fall_multiplier - 1) * Time.deltaTime;
-        }
-        else if (rigidBody2D.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
-            rigidBody2D.velocity += Vector2.up * Physics2D.gravity * (low_jump_multiplier - 1) * Time.deltaTime;
-        }
-        //if (rigidBody2D.velocity.magnitude < max_speed) {
-        //    Vector2 movement = new Vector2(x_movement, 0);
-        //    rigidBody2D.AddForce(movement_scalar * movement);
-        //}
     }
 }
