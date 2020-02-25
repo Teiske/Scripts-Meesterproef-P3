8ef75b75d539396d35109c2ddf1f01c90621a111
Teis <teisgossen@gmail.com>
2020-02-25 13:00:55 +0100

    Better Jumping
    + Player can only jump when grounded

--- Player_Movement_2D.cs
+++ Player_Movement_2D.cs
@@ -10,6 +10,11 @@ public class Player_Movement_2D : MonoBehaviour {
     [SerializeField] private float jump_Force;
     //[SerializeField] private float movement_scalar;
 
+    [SerializeField] private bool is_grounded = false;
+    [SerializeField] private Transform is_ground_checker;
+    [SerializeField] private float check_ground_radius;
+    [SerializeField] private LayerMask ground_layer;
+
     // Start is called before the first frame update
     void Start() {
         rigidBody2D = this.gameObject.GetComponent<Rigidbody2D>();
@@ -28,10 +33,18 @@ public class Player_Movement_2D : MonoBehaviour {
 
         rigidBody2D.velocity = new Vector2(move_by, rigidBody2D.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && is_grounded) {
             rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jump_Force);
         }
 
+        Collider2D collider = Physics2D.OverlapCircle(is_ground_checker.position, check_ground_radius, ground_layer);
+
+        if (collider != null) {
+            is_grounded = true;
+        }
+        else {
+            is_grounded = false;
+        }
         //if (rigidBody2D.velocity.magnitude < max_speed) {
         //    Vector2 movement = new Vector2(x_movement, 0);
         //    rigidBody2D.AddForce(movement_scalar * movement);
