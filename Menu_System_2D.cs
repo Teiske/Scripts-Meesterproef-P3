5baa03d05d660c930accc2a46fb1e7c89f686e6c
Teis <teisgossen@gmail.com>
2020-05-19 23:47:55 +0200

    Menu System
    + Added a wortking menu system

---
+++ Menu_System_2D.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu_System_2D : MonoBehaviour {
+
+    [SerializeField] private Canvas quit_menu;
+
+    [SerializeField] private Button start_button;
+    [SerializeField] private Button exit_button;
+
+    // Start is called before the first frame update
+    void Start() {
+        quit_menu = quit_menu.GetComponent<Canvas>();
+
+        start_button = start_button.GetComponent<Button>();
+        exit_button = exit_button.GetComponent<Button>();
+
+        quit_menu.enabled = false;
+    }
+
+    public void ExitPress() {
+        quit_menu.enabled = true;
+        start_button.enabled = false;
+        exit_button.enabled = false;
+    }
+
+    public void NoPress() {
+        quit_menu.enabled = false;
+        start_button.enabled = true;
+        exit_button.enabled = true;
+    }
+
+    public void StartLevel() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
---
+++ Pause_Menu_System_2D.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu_System_2D : MonoBehaviour {
+
+    [SerializeField] private Canvas pause_menu;
+
+    [SerializeField] private Button Continue_button;
+    [SerializeField] private Button quitToMenu_button;
+    [SerializeField] private Button exit_button;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        pause_menu = pause_menu.GetComponent<Canvas>();
+       
+        Continue_button = Continue_button.GetComponent<Button>();
+        quitToMenu_button = quitToMenu_button.GetComponent<Button>();
+        exit_button = exit_button.GetComponent<Button>();
+
+        pause_menu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            pause_menu.enabled = !pause_menu.enabled;
+        }
+    }
+
+    public void StartLevel() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitToMenuPress() {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void ContinuePress() {
+
+        pause_menu.enabled = false;
+
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
